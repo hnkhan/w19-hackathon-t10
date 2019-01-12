c120966b8587f12db3f38e3a71db032c69e24f00
Tim Tran <ttran1@ualberta.ca>
2019-01-12 13:15:13 -0700

    html and js

---
+++ app/src/js/bigbrother.js
@@ -0,0 +1,7 @@
+var percentPos = 0.5;
+var percentNeutral = 0.3;
+var percentNeg = 0.2;
+
+$("#pos").text("Positive: " + percentPos + "%");
+$("#neg").text("Negative: " + percentNeg + "%");
+$("#neutral").text("Neutral: " + percentNeutral + "%");
