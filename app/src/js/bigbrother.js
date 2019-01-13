8c9201eb0392ff212bd54cd0d41c0743b976001a
Tim Tran <ttran1@ualberta.ca>
2019-01-12 17:19:14 -0700

    fix css bug

--- app/src/js/bigbrother.js
+++ app/src/js/bigbrother.js
@@ -69,6 +69,12 @@ function generatePie() {
 
 d3.select("#enter")
     .on("click", function () {
+        // Make a request for a user with a given ID
+        axios.get('http://127.0.0.1:8000/sentimentanalysis/form')
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
         populateTextFields();
         if (pieGenerated) {
             update();
